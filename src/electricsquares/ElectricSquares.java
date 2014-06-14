538dec54a0835b847145d8c82653d1c8b0c1248e
Blades333 <blademaster333@gmail.com>
2014-06-14 16:11:47 -0400

    Second

---
+++ src/electricsquares/ElectricSquares.java
@@ -0,0 +1,13 @@
+package electricsquares;
+
+public class ElectricSquares {
+
+  ElectricSquares(){
+    System.out.println("Hello World");
+  }
+  
+  public static void main(String[] args) {
+    new ElectricSquares();
+  }
+  
+}
